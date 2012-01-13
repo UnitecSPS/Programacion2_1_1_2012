72e979479809319c619dc838cbbf0a8f9ff69afe
Carlos Gochez <carlos.gochez@unitec.edu>
2012-01-13 08:32:40 -0600

    agregue algo nuevo

---
+++ src/programacion2_1_1_2012/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion2_1_1_2012;
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Carlos Armando Gochez");
+    }
+}
