64a749f258792336207487b0cee9feb9535abde0
Carlos Gochez <carlos.gochez@unitec.edu>
2012-03-02 09:32:51 -0600

    agregar carros y mas

--- src/AutoLote/AutoControl.java
+++ src/AutoLote/AutoControl.java
@@ -5,9 +5,8 @@
 //package AutoLote;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -21,8 +20,93 @@ public class AutoControl {
         try {
             ramAuto = new RandomAccessFile("autos.alt","rw");
             ramFacturas = new RandomAccessFile("facturas.alt","rw");
+            inicializarCodigos();
         } catch (FileNotFoundException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
+    }
+
+    private void inicializarCodigos() {
+        try{
+           RandomAccessFile ramCods = new RandomAccessFile("codigos.alt", "rw");
+           
+           if( ramCods.length() == 0 ){
+               //es nuevo
+               ramCods.writeInt(1);//cods carros
+               ramCods.writeInt(1);//cod facturas
+               
+               ramCods.close();
+           }
+        }
+        catch(IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    
+    private int getCodigoSecuencial(int offset) throws IOException{
+        RandomAccessFile ramCods = new RandomAccessFile("codigos.alt", "rw");
+        ramCods.seek(offset);
+        
+        int codigo = ramCods.readInt();
+        //dejar actualizado
+        ramCods.seek(offset);
+        ramCods.writeInt( codigo + 1);
+        ramCods.close();
+        
+        return codigo;
+        
+    }
+    
+    private int getCodigoCarro() throws IOException{
+        return getCodigoSecuencial(0);
+    }
+    
+    private int getCodigoFactura() throws IOException{
+        return getCodigoSecuencial(4);
+    }
+    
+    public void adicionarCarro(String d,boolean m,double p,int a) throws IOException{
+        ramAuto.seek(ramAuto.length());
+        
+        //formato a guardar
+        ramAuto.writeInt( this.getCodigoCarro() );
+        ramAuto.writeUTF(d); //descripcion
+        ramAuto.writeBoolean(m); //mecanico o no
+        ramAuto.writeDouble(p); //precio
+        ramAuto.writeInt(a); //anio
+        ramAuto.writeBoolean(true); //dispo
+    }
+    
+    public void imprimirAutosDisponibles() throws IOException{
+        ramAuto.seek(0);
+        
+        while( ramAuto.getFilePointer() < ramAuto.length() ){
+            int cod = ramAuto.readInt();
+            String d = ramAuto.readUTF();
+            boolean m = ramAuto.readBoolean();
+            double prec = ramAuto.readDouble();
+            int anio = ramAuto.readInt();
+            
+            if( ramAuto.readBoolean() ){
+                System.out.println(cod + "-" + d + (m ? " Mecanico " : " Automatico ") +
+                        " Lps " + prec + " anio " + anio);
+            }
+        }
+    }
+    
+    public long buscarAuto(int codAuto) throws IOException{
+        ramAuto.seek(0);
+        
+        while( ramAuto.getFilePointer() < ramAuto.length() ){
+            long pos = ramAuto.getFilePointer();
+            int cod = ramAuto.readInt();
+            ramAuto.readUTF();
+            ramAuto.seek( ramAuto.getFilePointer() + 13 );
+            
+            if( ramAuto.readBoolean() && cod == codAuto )
+                return pos;
+        }
+        
+        return -1;
     }
 }
--- src/AutoLote/EligeTuCarro.java
+++ src/AutoLote/EligeTuCarro.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -11,5 +14,51 @@
 public class EligeTuCarro {
     public static void main(String args[]){
         AutoControl at = new AutoControl();
+        Scanner lea = new Scanner(System.in);
+        
+        int op;
+        
+        do{
+            System.out.println("\n\nMENU PRINCIPAL\n-------");
+            System.out.println("1- Ingrese Auto");
+            System.out.println("2- Imprimir Autos Disponibles");
+            System.out.println("3- Buscar Auto");
+            System.out.println("4- Salir");
+            System.out.print("\nEscoja opcion: ");
+            op = lea.nextInt();
+            
+            try{
+                switch( op ){
+                    case 1:
+                        System.out.print("Descripcion: ");
+                        String d = lea.next();
+                        System.out.print("Mecanico (s/n): ");
+                        char mec = lea.next().charAt(0);
+                        System.out.print("Precio: ");
+                        double p = lea.nextDouble();
+                        System.out.print("Anio del Carro: ");
+                        int a = lea.nextInt();
+                        
+                        at.adicionarCarro(d,mec == 's' ? true : false, p, a);
+                        break;
+                    case 2:
+                        at.imprimirAutosDisponibles();
+                        break;
+                    case 3:
+                        System.out.print("Ingrese Codigo: ");
+                        long result = at.buscarAuto( lea.nextInt() );
+                        if( result > -1 )
+                            System.out.println("Si ESTA Disponible");
+                        else
+                            System.out.println("No existe o NO esta disponible");
+                        break;
+                        
+                }
+            }
+            catch(Exception e ){
+                System.out.println("Error: " + e.getMessage());
+            }
+        }while( op != 4 );
+        
     }
 }
